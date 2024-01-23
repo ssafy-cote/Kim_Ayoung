0aec6c69348f0db46b4d1fad4aa9911252fc6030
ayoung kim <ayoung7777@gmail.com>
2024-01-23 10:03:35 +0900

    백준 - 연속합

---
+++ 1912. 연속합/연속합.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	final static int min = -10000;
+	static int N;
+	static int[] arrayNum;
+	static int max = min;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		arrayNum = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arrayNum[i] = sc.nextInt();
+			max = Math.max(max, arrayNum[i]);
+		}
+		
+		int sum = 0;
+		for(int i = 0; i < N; i++) {
+			if(i != 0 && sum <= 0) sum = 0;
+			sum += arrayNum[i];
+			max = Math.max(max, sum);
+		}
+		
+		System.out.println(max);
+		sc.close();
+	}
+}
